8351236b071da5994fa27bca86d7e4fe4707dda8
Bruno <ask@bruno.wtf>
2024-08-26 15:35:00 +0200

    display error message

--- source/web/pages/Yubikey/Yubikey.tsx
+++ source/web/pages/Yubikey/Yubikey.tsx
@@ -55,6 +55,7 @@ const YUBIKEY_ERROR_STRINGS = {
     YUBIKEY_BRIDGE_NOT_INSTALLED: 'native messaging host not found',
     YUBIKEY_BRIDGE_FAILED: 'Native host has exited',
     YUBIKEY_BRIDGE_NOT_IMPLEMENTED: 'Yubikey Bridge error: Not implemented',
+    YUBIKEY_NO_SIGNATURE_KEY: 'No signature key on Yubikey',
     CANNOT_CONNECT_TO_YUBIKEYWALLET: '',
     NO_YUBIKEY_FOUND: '',
     NO_GPGKEY_ON_YUBIKEY: '',
@@ -157,6 +158,8 @@ const YubikeyPage = () => {
             errorMessage = ALERT_MESSAGES_STRINGS.YUBIKEYWALLET_NOT_INSTALLED;
         } else if (error.includes(YUBIKEY_ERROR_STRINGS.YUBIKEY_BRIDGE_NOT_IMPLEMENTED)) {
             errorMessage = ALERT_MESSAGES_STRINGS.YUBIKEY_BRIDGE_NOT_IMPLEMENTED;
+        } else if (error.includes(YUBIKEY_ERROR_STRINGS.YUBIKEY_NO_SIGNATURE_KEY)) {
+            errorMessage = error;
         } else if (error.includes(YUBIKEY_ERROR_STRINGS.YUBIKEY_BRIDGE_FAILED)) {
             errorMessage = ALERT_MESSAGES_STRINGS.YUBIKEYWALLET_FAILED;
         } else if (error.includes(YUBIKEY_ERROR_STRINGS.CANNOT_CONNECT_TO_YUBIKEYWALLET)) {
